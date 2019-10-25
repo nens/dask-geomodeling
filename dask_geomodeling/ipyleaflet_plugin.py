450431eebb0b36d8187d54eff9a1c4a121a049f3
Casper van der Wel <caspervdw@gmail.com>
2019-10-25 16:37:08 +0200

    Finish plugin

--- dask_geomodeling/ipyleaflet_plugin.py
+++ dask_geomodeling/ipyleaflet_plugin.py
@@ -1,27 +1,26 @@
-try:
-    from notebook.utils import url_path_join
-    from notebook.base.handlers import IPythonHandler
-    from ipyleaflet import WMSLayer
-except ImportError:
-    url_path_join = None
-    IPythonHandler = object
-    WMSLayer = None
-    raise
-
-from dask_geomodeling.core import Block
+import os
 from datetime import datetime
+from io import BytesIO
+
+import numpy as np
+import traitlets
+from dask import config
+from ipyleaflet import Map, WMSLayer
 from matplotlib import cm
 from matplotlib.colors import Normalize
-from io import BytesIO
-import numpy as np
+from notebook import notebookapp
+from notebook.base.handlers import IPythonHandler
+from notebook.utils import url_path_join
 from PIL import Image
-import traitlets
+
+from dask_geomodeling.core import Block
+
+config.set({"geomodeling.root": os.getcwd()})
+
 
 # include this plugin with:
 # jupyter notebook --NotebookApp.nbserver_extensions="{'dask_geomodeling.ipyleaflet_plugin':True}"
-class HelloWorldHandler(IPythonHandler):
-    def get(self):
-        self.finish("Hello world!")
+# or run jupyter notebook --generate-config and set it in the config file in ~/.jupyter
 
 
 class GeomodelingWMSHandler(IPythonHandler):
@@ -39,8 +38,9 @@ class GeomodelingWMSHandler(IPythonHandler):
             return
         srs = self.get_query_argument("srs")
         height = int(self.get_query_argument("height"))
-        max_cell_size = 5  # TODO
-        time = datetime.utcnow()  # TODO datetime.fromisoformat(self.get_query_argument("time"))
+        max_cell_size = 10
+        time = datetime.utcnow()
+        # TODO datetime.fromisoformat(self.get_query_argument("time"))
         width = int(self.get_query_argument("width"))
         bbox = [float(x) for x in self.get_query_argument("bbox").split(",")]
 
@@ -66,7 +66,7 @@ class GeomodelingWMSHandler(IPythonHandler):
 
         normalized = Normalize(vmin=vmin, vmax=vmax, clip=True)(masked)
         img = cm.get_cmap(style)(normalized)
-        img[normalized.mask, 3] = 0.
+        img[normalized.mask, 3] = 0.0
         img_uint8 = (img * 255).astype(np.uint8)
         Image.fromarray(img_uint8).save(stream, format="png")
         raw = stream.getvalue()
@@ -74,38 +74,39 @@ class GeomodelingWMSHandler(IPythonHandler):
         self.set_header("Content-Length", len(raw))
         self.set_header("Content-Type", "image/png")
         self.set_header("Pragma", "no-cache")
-        self.set_header("Cache-Control",
-                        "no-store, "
-                        "no-cache=Set-Cookie, "
-                        "proxy-revalidate, "
-                        "max-age=0, "
-                        "post-check=0, pre-check=0")
+        self.set_header(
+            "Cache-Control",
+            "no-store, "
+            "no-cache=Set-Cookie, "
+            "proxy-revalidate, "
+            "max-age=0, "
+            "post-check=0, pre-check=0",
+        )
         self.set_header("Expires", "Wed, 2 Dec 1837 21:00:12 GMT")
         self.write(raw)
         self.finish()
 
 
 class GeomodelingLayer(WMSLayer):
-    base_url = None  # set by load_jupyter_server_extension
+    """Visualize a dask_geomodeling.RasterBlock on a ipyleaflet Map
 
-    format = traitlets.Unicode('image/png').tag(sync=True, o=True)
+    :param block: a dask_geomodeling.RasterBlock instance to visualize
+    :param hostname: The hostname of the jupyter server
+    :param style: a valid matplotlib colormap
+    :param vmin: the minimum value (for the colormap)
+    :param vmax: the maximum value (for the colormap)
+    """
+    format = traitlets.Unicode("image/png").tag(sync=True, o=True)
     vmin = traitlets.Float(0.0).tag(sync=True, o=True)
     vmax = traitlets.Float(1.0).tag(sync=True, o=True)
 
-    def __init__(self, hostname="localhost", **kwargs):
-        from notebook import notebookapp
-
-        self.format = "image/png"
-
+    def __init__(self, block, hostname="localhost", **kwargs):
         for server in notebookapp.list_running_servers():
             if server["hostname"] == hostname:
                 kwargs["url"] = server["url"] + "wms"
                 break
-
-        super().__init__(**kwargs)
-
-    def set_block(self, block):
         self.layers = block.to_json()
+        super().__init__(**kwargs)
 
 
 def load_jupyter_server_extension(nb_server_app):
@@ -116,8 +117,6 @@ def load_jupyter_server_extension(nb_server_app):
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
     web_app = nb_server_app.web_app
-    host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
-    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
-    route_pattern = url_path_join(web_app.settings['base_url'], '/wms')
+    host_pattern = ".*$"
+    route_pattern = url_path_join(web_app.settings["base_url"], "/wms")
     web_app.add_handlers(host_pattern, [(route_pattern, GeomodelingWMSHandler)])
