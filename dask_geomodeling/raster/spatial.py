f2376d35889b3469d2f409195fb3f3984b88580b
Arjan Verkerk <arjan.verkerk@gmail.com>
2024-07-01 14:29:27 +0200

    Remove breakpoint.

--- dask_geomodeling/raster/spatial.py
+++ dask_geomodeling/raster/spatial.py
@@ -728,7 +728,4 @@ class Place(BaseSingle):
                 "no_data_value": out_no_data_value,
             }
         else:
-            if stack[0]['values'].ndim == 4:
-                import pdb
-                pdb.set_trace() 
             return reduce_rasters(stack, process_kwargs["statistic"])
