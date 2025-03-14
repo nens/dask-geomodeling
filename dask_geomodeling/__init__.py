9a5ea95acfc0793067aa83d1aa7372c3ea8cb161
Arjan Verkerk <arjan.verkerk@gmail.com>
2025-03-14 16:07:52 +0100

    Attempt to fix the conda builds.

--- dask_geomodeling/__init__.py
+++ dask_geomodeling/__init__.py
@@ -4,6 +4,8 @@ gdal.UseExceptions()
 ogr.UseExceptions()
 osr.UseExceptions()
 
+gdal.SetConfigOption("GDAL_MEM_ENABLE_OPEN", "YES")
+
 from . import config  # NOQA
 from .core import Block, construct  # NOQA
 from . import raster  # NOQA
